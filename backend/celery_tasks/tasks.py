ca7ac97eca01a4a19584a8691c10205ac42f95a9
itimor <itimor@126.com>
2020-06-15 16:33:16 +0800

    add

--- backend/celery_tasks/tasks.py
+++ backend/celery_tasks/tasks.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 from datetime import datetime, timedelta
 from celery_tasks.models import *
 from django_celery_beat.models import CrontabSchedule, PeriodicTask
+import logging
 
 
 @shared_task
@@ -19,9 +20,12 @@ def get_task():
     start = now - timedelta(hours=23, minutes=59, seconds=59)
     all_task = Task.objects.filter(status=True, start_time__lt=start, expire_time__gt=start)
     for task in all_task:
+        logging.error(task.name)
         iter = croniter(task.cron, now)
         t = iter.get_next(datetime)
+        logging.error(t)
         cron_obj, created = CrontabSchedule.objects.get_or_create(minute=t.minute, hour=t.hour, day_of_month=t.month, month_of_year=t.year)
+        logging.error(cron_obj)
         PeriodicTask.objects.create(
             name=task.name,
             task='celery_tasks.tasks.run_task',
